26625f47b4bf75dbef0c9276164751efc44ae81c
keriber <keria.bermudez@gmail.com>
2014-08-06 14:54:12 -0400

    Area
    These are series of pythons scripts to organize the area data of the
    ROIs

---
+++ Step7_Area_CSV/Cre_Bax/Creat_Hilar_Area_CSV_Cre_Bax.py
@@ -0,0 +1,43 @@
+ # -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 14:52:00 2013
+
+@author: kbermudez-hernandez
+"""
+
+import os
+import numpy as np
+from pandas import Series, DataFrame
+import pandas as pd
+import re
+#Code to make list of files and vdevide them in slide1  and slide2
+path = "F:\Backup Seagate Hardrive\CreBax\CSV_Files"
+os.chdir(path)#change directory
+from pandas import Series, DataFrame
+
+
+FileNames=[]
+Values = []
+for file in os.listdir(path):
+    if file.endswith(".txt"):
+        FileNames.append(os.path.basename(file)[:-4])
+        Value = pd.read_table(file,sep='\t')
+        Values.append(Value)
+
+Data = dict(zip(FileNames,Values)) 
+globals().update(Data)
+
+
+                     
+for file in FileNames:
+    Data[file] = Data[file].rename(columns= {'Label':'SectionsID', 'Area':'Hilar Area'})
+    #Data[file]['Hilar Area']= Data[file]['Hilar Area'] * 0.136342782003905 
+    Data[file].SectionsID = Data[file].SectionsID.str[:-4]
+    X =  Data[file].SectionsID.str[0:2]
+    Data[file].index = X.astype(int)
+    Data[file].index.name ='Sections'
+
+for file in FileNames:
+    Data[file].to_csv(file + '_Area'+'.csv')
+
+     
---
+++ Step7_Area_CSV/Strains/Creat_SGZ_Area_P16_P30_P60.py
@@ -0,0 +1,43 @@
+ # -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 14:52:00 2013
+
+@author: kbermudez-hernandez
+"""
+
+import os
+import numpy as np
+from pandas import Series, DataFrame
+import pandas as pd
+import re
+#Code to make list of files and vdevide them in slide1  and slide2
+path = "F:\Backup Seagate Hardrive\Quantification_C57_SW\Quantification_Default_GCL_H\SGZ_CSVs"
+os.chdir(path)#change directory
+from pandas import Series, DataFrame
+
+
+FileNames=[]
+Values = []
+for file in os.listdir(path):
+    if file.endswith(".txt"):
+        FileNames.append(os.path.basename(file)[:-4])
+        Value = pd.read_table(file,sep='\t')
+        Values.append(Value)
+
+Data = dict(zip(FileNames,Values)) 
+globals().update(Data)
+
+
+                     
+for file in FileNames:
+    Data[file] = Data[file].rename(columns= {'Label':'SectionsID', 'Area':'SGZ Area'})
+    #Data[file]['Hilar Area']= Data[file]['Hilar Area'] * 0.136342782003905 
+    Data[file].SectionsID = Data[file].SectionsID.str[:-4]
+    X =  Data[file].SectionsID.str[0:2]
+    Data[file].index = X
+    Data[file].index.name ='Sections'
+
+for file in FileNames:
+    Data[file].to_csv(file + '_Area'+'.csv')
+
+     ##Data[file].index = X.astype(int)
